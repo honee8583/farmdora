d32afa80b49fad71428a71077eb69787b8a93caf
leedahoon <ekgns0508@gmail.com>
2025-08-25 14:45:06 +0900

    project init

---
+++ src/main/java/com/farmdora/farmdora/FarmdoraApplication.java
@@ -0,0 +1,13 @@
+package com.farmdora.farmdora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmdoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmdoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmdora/farmdora/FarmdoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmdora.farmdora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmdoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
