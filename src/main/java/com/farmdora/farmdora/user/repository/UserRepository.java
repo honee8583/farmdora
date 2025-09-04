997ea5b2c9c381031bdc9972dd4f592e0c18b67f
leedahoon <ekgns0508@gmail.com>
2025-09-04 21:59:10 +0900

    feat: User 레포지토리 추가

---
+++ src/main/java/com/farmdora/farmdora/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.farmdora.farmdora.user.repository;
+
+import com.farmdora.farmdora.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
